e2220d10f0ef8f2b3aff6a2f96789d15a4107592
Rafael12eFaixa <rafael.andriotti8@gmail.com>
2024-09-23 14:04:36 -0300

    Teste 3

--- routes/agendaRoutes.js
+++ routes/agendaRoutes.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const agendaController = require('../controllers/agendaController');
-const salaController = require('../controllers/agendaController');
-const equipamentoController = require('../controllers/agendaController');
-const professorController = require('../controllers/agendaController');
 // lembrando que a rota raiz tem clientes, definido no app.js
 // Rota para obter todos os clientes
 router.get('/', agendaController.getAllAgenda);
@@ -14,28 +11,10 @@ router.put('/:id', agendaController.updateAgenda);
 // Rota para deletar um cliente
 router.delete('/:id', agendaController.deleteAgenda);
 
-router.get('/', salaController.getAllSala);
-// Rota para criar um novo cliente
-router.post('/', salaController.createSala);
-// Rota para atualizar um cliente existente
-router.put('/:id', salaController.updateSala);
-// Rota para deletar um cliente
-router.delete('/:id', salaController.deleteSala);
 
-router.get('/', equipamentoController.getAllEquipamento);
-// Rota para criar um novo cliente
-router.post('/', equipamentoController.createEquipamento);
-// Rota para atualizar um cliente existente
-router.put('/', equipamentoController.updateEquipamento);
-// Rota para deletar um cliente
-router.delete('/', equipamentoController.deleteEquipamento);
 
-router.get('/', professorController.getAllProfessor);
-// Rota para criar um novo cliente
-router.post('/', professorController.createProfessor);
-// Rota para atualizar um cliente existente
-router.put('/:id', professorController.updateProfessor);
-// Rota para deletar um cliente
-router.delete('/:id', professorController.deleteProfessor);
+
+
+
 
 module.exports = router;
---
+++ routes/salaRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const salaController = require('../controllers/agendaController');
+
+router.get('/', salaController.getAllSala);
+// Rota para criar um novo cliente
+router.post('/', salaController.createSala);
+// Rota para atualizar um cliente existente
+router.put('/:id', salaController.updateSala);
+// Rota para deletar um cliente
+router.delete('/:id', salaController.deleteSala);
+
+module.exports = router;

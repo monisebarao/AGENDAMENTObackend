c004131448034ce8f28b4f332fdef31ed7de63e3
Rafael12eFaixa <rafael.andriotti8@gmail.com>
2024-09-24 17:22:31 -0300

    Isabela cara de muçarela

--- models/sala.js
+++ models/sala.js
@@ -1,14 +1,52 @@
-const sqlite3 = require("sqlite3").verbose();
-const dbPath = './infra/database.db';
-// Função para abrir conexão com o banco de dados
+const { Request, TYPES } = require("tedious");
 
-function openDbConnection() {
-  let db = new sqlite3.Database(dbPath, sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-      console.error("Erro ao abrir o banco de dados:", err.message);
-    }
+// Importa a função que conecta ao banco de dados
+const connectDatabase = require("../database/connection");
+
+// Função genérica para executar uma query SQL
+async function executeQuery(query, params = []) {
+  // Estabelece uma conexão com o banco de dados
+  const connection = await connectDatabase();
+  
+  // Retorna uma Promise para lidar com a execução assíncrona da query
+  return new Promise((resolve, reject) => {
+    // Cria uma nova requisição SQL com a query passada e um callback para erros
+    const request = new Request(query, (err) => {
+      if (err) {
+        // Se ocorrer um erro, a Promise é rejeitada e a conexão é fechada
+        reject(err);
+        connection.close();
+      }
+    });
+
+    // Adiciona parâmetros à requisição SQL (nome, tipo e valor)
+    params.forEach(({ name, type, value }) => {
+      request.addParameter(name, type, value);
+    });
+
+    // Array para armazenar os resultados retornados pela query
+    let results = [];
+
+    // Evento "row" é disparado para cada linha retornada pela query
+    request.on("row", (columns) => {
+      // Cria um objeto para cada linha e armazena suas colunas e valores
+      let row = {};
+      columns.forEach((column) => {
+        row[column.metadata.colName] = column.value;
+      });
+      results.push(row);
+    });
+
+    // Evento "requestCompleted" é disparado quando a query é completamente executada
+    request.on("requestCompleted", () => {
+      // Fecha a conexão com o banco de dados e resolve a Promise com os resultados
+      connection.close();
+      resolve(results);
+    });
+
+    // Executa a requisição SQL
+    connection.execSql(request);
   });
-  return db;
 }
 
 function getAllSala(callback) {
